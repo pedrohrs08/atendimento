bf47a4ae8444f7746ce381ddf45f6c20b2c0b0b6
Pedro Soares <psoares@avenuecode.com>
2016-08-03 16:13:53 -0300

    commiting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require websocket_rails/main
 
 $(function(){
-	var dispatcher = new WebSocketRails(window.location.host + '/websocket');
+	var dispatcher = new WebSocketRails("52.43.104.250:3000" + '/websocket');
 
 
 	dispatcher.on_open = function(data) {  
@@ -28,14 +28,25 @@ $(function(){
   		    }
   			console.log(channel);
 			channel.bind('new', function(post) {
-			  var url = "http://doadores-env.ewmarezs3y.sa-east-1.elasticbeanstalk.com/donors/"	
-			  url = url + post["id"]
-			  $.get( url, function( data ) {
-				  $("#requests").append("<label id='donor'>" + data["name"] + "</label>" +
-                    "<label id='reason'>" + post["reason"] + "</label>" +
-                    "<a href='#'>Atender e Faturar</a>"
-				  	"<br>");
-			  });
+		      console.log(post);
+			  post = JSON.parse(post);
+			  var url = "http://doadores-env.ewmarezs3y.sa-east-1.elasticbeanstalk.com/donors/";	
+			  url = url + post["requester"] + ".json";
+			  $.ajax({ 
+			  		type: 'GET', 
+            		crossDomain: true,
+            		url: url,
+            		error: function(error){
+ 						console.log(error);
+            		},
+            		success: function( data ) {
+				  		$("#requests").append("<div style='border: 1px solid black'>" +
+				  			"<label id='donor'>" + data["name"] + "</label><br>" +
+                    		"<label id='reason'>" + post["reason"] + "</label><br>" +
+                    		"<a href='#'>Atender e Faturar</a>" +
+				  		"</div>");
+						}
+					});
 			});
 	}
 
